49e27608652d26a4193f8a8f038d0fe36cc4ad97
cynwachi <christinenwachi@gmail.com>
2020-10-26 10:21:53 -0500

    starting

--- django_music/settings.py
+++ django_music/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 
     # Third-party
     'debug_toolbar',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
     # Project-specific
     'users',
+    'albums',
 ]
 
 MIDDLEWARE = [
